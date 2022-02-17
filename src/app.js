7971e9093a48aec46e0eba09b989c8d55926585d
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2022-02-17 17:54:06 -0600

    commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { __dirname } from './utils.js';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname+'/public'))
+
+app.get('/info',(req,res)=>{
+    res.send({PORT:PORT})
+})
+
+const server = app.listen(PORT,()=>console.log(`Listening on PORT ${PORT}`))
